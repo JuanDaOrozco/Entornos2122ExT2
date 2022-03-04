072913fc134d2383b67767cdcb624d9b211bc22a
Juan David Orozco Torres <juand.orozcotorres@iesfm.onmicrosoft.com>
2022-03-04 11:23:23 +0100

    Refactorizacion del Problema 1, correccion en el expected del assert del test y preparacion del Problema 2

--- test/TestProblema2.java
+++ test/TestProblema2.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestProblema1 {
+public class TestProblema2 {
     Problema1 fizz = new Problema1();
     @BeforeEach
     private void setUp() {
@@ -10,6 +10,6 @@ public class TestProblema1 {
     }
     @Test
     public void testProblema1() {
-        Assertions.assertEquals(  "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19Buzz", fizz.iniciar());
+        Assertions.assertEquals(  "12Fizz4BuzzFizz78FizzBuzz11Fizz1314Fizz1617Fizz19Buzz", fizz.iniciar());
     }
 }
