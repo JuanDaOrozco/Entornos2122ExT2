09248cd1c93f5ddbcbecb913c9c01e1a91bd5d05
Juan David Orozco Torres <juand.orozcotorres@iesfm.onmicrosoft.com>
2022-03-04 11:50:03 +0100

    Resolucion del Problema 3

--- src/Problema3.java
+++ src/Problema3.java
@@ -3,14 +3,35 @@ public class Problema3 {
         int number = in;
         String text = "";
         for (int i = in; i <= fi; i++) {
-            if (number % 3 == 0 && number % 5 == 0) {
-                text = text + "FizzBuzz";
-            }
-            else if (number % 3 == 0) {
+            if (number % 3 == 0) {
                 text = text + "Fizz";
+                if (number % 5 == 0) {
+                    text = text + "Buzz";
+                }
+                else if (number % 7 == 0) {
+                    text = text + "Foo";
+                }
+                else if (number % 11 == 0) {
+                    text = text + "Boo";
+                }
             }
             else if (number % 5 == 0) {
                 text = text + "Buzz";
+                if (number % 7 == 0) {
+                    text = text + "Foo";
+                }
+                else if (number % 11 == 0) {
+                    text = text + "Boo";
+                }
+            }
+            else if (number % 7 == 0) {
+                text = text + "Foo";
+                if (number % 11 == 0) {
+                    text = text + "Boo";
+                }
+            }
+            else if (number % 11 == 0) {
+                text = text + "Boo";
             }
             else {
                 text = text + number;
--- test/TestProblema3.java
+++ test/TestProblema3.java
@@ -3,13 +3,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestProblema3 {
-    Problema2 fizz = new Problema2();
+    Problema3 fizz = new Problema3();
     @BeforeEach
     private void setUp() {
-        fizz = new Problema2();
+        fizz = new Problema3();
     }
     @Test
     public void testProblema1() {
-        Assertions.assertEquals(  "BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19Buzz", fizz.iniciar(5, 20));
+        Assertions.assertEquals(  "BuzzBooFizz13FooFizzBuzz1617Fizz19BuzzFizzFoo", fizz.iniciar(10, 21));
     }
 }
